8ecf5514e517730c17e3a14a53a02aa015893a93
votuongquan2004@gmail.com <votuongquan2004@gmail.com>
2024-05-07 02:13:32 +0700

    update data path

--- wholepose/demo.py
+++ wholepose/demo.py
@@ -268,7 +268,7 @@ def main():
         #     transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         # ])
 
-        input_path = 'videos_batch1'
+        input_path = '/kaggle/input/vsl-rgb-videos/Updated_videos'
         paths = []
         names = []
         for root, _, fnames in natsorted(os.walk(input_path)):
